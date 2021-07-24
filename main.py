34f3b5531d013a9721c2ee8fb8fdf1f936e5970c
Abhishek <laios.shado@gmail.com>
2021-07-24 17:45:24 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,60 @@
+import pygame
+import numpy as np
+import os
+import grid
+
+# to center the window
+os.environ["SDL_VIDEO_CENTERED"]='1'
+
+#resolution
+width, height = 1200, 700
+size = (width, height)
+
+pygame.init()
+pygame.display.set_caption("CONWAY'S GAME OF LIFE")
+screen = pygame.display.set_mode(size)
+
+# to control the frame rate
+clock = pygame.time.Clock()
+fps = 28
+
+# colors in RGB notation
+black = (0, 0, 0)
+blue = (0, 25, 90)
+white = (255, 255, 255)
+
+px_scale = 15     # size of pixels
+offset = 2      # offset between pixels
+
+Grid = grid.Grid(width, height, px_scale, offset)   # creating the grid
+Grid.random2d_array()   # initializing grid with random cell values (either alive or dead)
+
+pause = False
+run = True
+
+while run:
+
+    clock.tick(fps)
+    screen.fill(black)
+
+    # controls when an input is provided
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_ESCAPE:
+                run = False
+            if event.key == pygame.K_SPACE:
+                pause = not pause
+    
+    # 
+    Grid.Conway(off_color=white, on_color=blue, surface=screen, pause=pause)
+
+    if pygame.mouse.get_pressed()[0]:
+        mouseX, mouseY = pygame.mouse.get_pos()
+        Grid.HandleMouse(mouseX, mouseY)
+
+    # to update the grid and play as an animation
+    pygame.display.update()
+
+pygame.quit()
